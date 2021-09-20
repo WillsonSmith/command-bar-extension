79aaa62c412edfe067e14682bbf8391a4c6ab6ed
Willson Smith <me@willsonsmith.com>
2021-09-19 20:37:12 -0400

    command bar loads on CSF

--- dist/scripts/extension-ui.js
+++ dist/scripts/extension-ui.js
@@ -28,21 +28,52 @@ class ExtensionUi extends n {
   static get styles() {
     return r$3`
       :host {
-        display: block;
+        pointer-events: none;
+        display: flex;
         position: absolute;
         top: 0;
+        right: 0;
+        bottom: 0;
         left: 0;
+
+        justify-content: center;
+        align-items: center;
       }
     `;
   }
 
+  static get properties() {
+    return {
+      active: { attribute: true, type: Boolean },
+      pressedKeys: { attribute: false },
+    };
+  }
+
   constructor() {
+    super();
+    this.pressedKeys = new Set();
+    window.addEventListener('keyup', (event) => {
+      this.pressedKeys.delete(event.key);
+    });
     window.addEventListener('keydown', (event) => {
-      console.log(event);
+      if (event.key === 'Escape') {
+        this.style.pointerEvents = 'none';
+        this.active = false;
+      }
+      this.pressedKeys.add(event.key);
+      if (
+        this.pressedKeys.has('Control') &&
+        this.pressedKeys.has('F') &&
+        this.pressedKeys.has('Shift')
+      ) {
+        this.style.pointerEvents = 'auto';
+        this.active = true;
+      }
     });
   }
 
   render() {
+    if (!this.active) return;
     return y` <command-bar></command-bar> `;
   }
 }
--- src/extension-ui.js
+++ src/extension-ui.js
@@ -4,21 +4,52 @@ export class ExtensionUi extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: block;
+        pointer-events: none;
+        display: flex;
         position: absolute;
         top: 0;
+        right: 0;
+        bottom: 0;
         left: 0;
+
+        justify-content: center;
+        align-items: center;
       }
     `;
   }
 
+  static get properties() {
+    return {
+      active: { attribute: true, type: Boolean },
+      pressedKeys: { attribute: false },
+    };
+  }
+
   constructor() {
+    super();
+    this.pressedKeys = new Set();
+    window.addEventListener('keyup', (event) => {
+      this.pressedKeys.delete(event.key);
+    });
     window.addEventListener('keydown', (event) => {
-      console.log(event);
+      if (event.key === 'Escape') {
+        this.style.pointerEvents = 'none';
+        this.active = false;
+      }
+      this.pressedKeys.add(event.key);
+      if (
+        this.pressedKeys.has('Control') &&
+        this.pressedKeys.has('F') &&
+        this.pressedKeys.has('Shift')
+      ) {
+        this.style.pointerEvents = 'auto';
+        this.active = true;
+      }
     });
   }
 
   render() {
+    if (!this.active) return;
     return html` <command-bar></command-bar> `;
   }
 }
